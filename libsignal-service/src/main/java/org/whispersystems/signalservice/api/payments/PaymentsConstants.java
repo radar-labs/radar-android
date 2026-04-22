97f461b44e7866ddf2303d294132c4e66a86e36f
Omar <omarh.ismail1@gmail.com>
2026-04-22 02:50:10 +0200

    change seed length to 12

--- libsignal-service/src/main/java/org/whispersystems/signalservice/api/payments/PaymentsConstants.java
+++ libsignal-service/src/main/java/org/whispersystems/signalservice/api/payments/PaymentsConstants.java
@@ -4,7 +4,7 @@ public final class PaymentsConstants {
 
   private PaymentsConstants() {}
 
-  public static final int PAYMENTS_ENTROPY_LENGTH = 32;
+  public static final int PAYMENTS_ENTROPY_LENGTH = 16;
   public static final int MNEMONIC_LENGTH         = Math.round(PAYMENTS_ENTROPY_LENGTH * 0.75f);
   public static final int SHORT_FRACTION_LENGTH   = 4;
 
